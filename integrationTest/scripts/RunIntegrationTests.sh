48d963301bbfcfb41374036316712aeea225fec9
tobi <tobi@backfrak.de>
2021-05-13 21:58:39 +0200

    Add integration tests

---
+++ integrationTest/scripts/RunIntegrationTests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+samba_exporter="/samba_exporter"
+samba_statusd="/samba_statusd"
+
+if [ -f "$samba_exporter" ]; then
+    echo "Run $samba_exporter"
+    $samba_exporter
+else
+    echo "Error $samba_exporter not found"
+    exit 1
+fi
+
+if [ -f "$samba_statusd" ]; then
+    echo "Run $samba_statusd"
+    $samba_statusd
+else
+    echo "Error $samba_statusd not found"
+    exit 1
+fi
+
+exit 0
