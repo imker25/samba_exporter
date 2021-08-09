d31fb88ef227e8ccc141e26cbe658575a9130366
tobi <tobi@backfrak.de>
2021-08-09 18:23:54 +0200

    Read packge values from the control file

--- build/GithubEnv.sh
+++ build/GithubEnv.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
+# ######################################################################################
+# Copyright 2021 by tobi@backfrak.de. All
+# rights reserved. Use of this source code is governed
+# by a BSD-style license that can be found in the
+# LICENSE file.
+# ######################################################################################
+# Script to read infos from files into the github environment during a actions workflow
+# ######################################################################################
 
+# Read package info from install/DEBIAN/control file
+controlFile="./install/DEBIAN/control"
+while IFS= read -r line
+do
+  if [[ $line = Description:* ]]; then
+    desc=${line/Description: /}
+    echo "SAMBA_EXPORTER_PACKAGE_DESCRIPTION=$desc"
+    echo "SAMBA_EXPORTER_PACKAGE_DESCRIPTION=$desc" >> $GITHUB_ENV 
+  fi
+  if [[ $line = Depends:* ]]; then
+    dep=${line/Depends: /}
+    echo "SAMBA_EXPORTER_PACKAGE_DEPENDS=$dep"
+    echo "SAMBA_EXPORTER_PACKAGE_DEPENDS=$dep" >> $GITHUB_ENV 
+  fi
+  if [[ $line = Maintainer:* ]]; then
+    dev=${line/Maintainer: /}
+    echo "SAMBA_EXPORTER_PACKAGE_MAINTAINER=$dev"
+    echo "SAMBA_EXPORTER_PACKAGE_MAINTAINER=$dev" >> $GITHUB_ENV 
+  fi  
+  if [[ $line = Package:* ]]; then
+    pack=${line/Package: /}
+    echo "SAMBA_EXPORTER_PACKAGE_NAME=$pack"
+    echo "SAMBA_EXPORTER_PACKAGE_NAME=$pack" >> $GITHUB_ENV 
+  fi   
+done < "$controlFile"
+
+# Read version infos from build log files
 echo " GITHUB_REF= $GITHUB_REF"
 packageName=$(cat logs/PackageName.txt)
 echo "SAMBA_EXPORTER_PACKAGE_NAME=$packageName"
