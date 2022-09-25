eaeae3bba34e8f549d196bc81505d0be782da08c
tobi <tobi@backfrak.de>
2022-09-25 20:03:36 +0200

    Add some debug code

--- src/tobi.backfrak.de/cmd/samba_exporter/main.go
+++ src/tobi.backfrak.de/cmd/samba_exporter/main.go
@@ -34,6 +34,23 @@ var logger commonbl.Logger
 
 func main() {
 	handleComandlineOptions()
+
+	// Use this to test the GetSmbdMetrics feature during development
+	// smbdMetrics, errSmbdMetric := statisticsGenerator.GetSmbdMetrics()
+	// if errSmbdMetric != nil {
+	// 	logger.WriteError(errSmbdMetric)
+	// 	return
+	// }
+
+	// for _, metric := range smbdMetrics {
+	// 	logger.WriteInformation(fmt.Sprintf("%s: %f", metric.Name, metric.Value))
+
+	// }
+
+	// if len(smbdMetrics) > 0 {
+	// 	return
+	// }
+
 	requestHandler := *commonbl.NewPipeHandler(params.Test, commonbl.RequestPipe)
 	responseHandler := *commonbl.NewPipeHandler(params.Test, commonbl.ResposePipe)
 	logger = *commonbl.NewLogger(params.Verbose)
